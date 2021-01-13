7f7e5277ca6ac3a1e3d027c31ef2766de4590483
Thor Anderson <ta.anderson@gmail.com>
2021-01-13 15:43:22 -0700

    Callback practice

---
+++ arrayPractice.js
@@ -0,0 +1,29 @@
+const items = [
+  {
+    id: '1',
+    name: 'One',
+  },
+  {
+    id: '2',
+    name: 'Two',
+  },
+]
+
+let newArray = []
+
+const results = items.forEach((item) => { // results is undefined
+  newArray.push(
+    {
+      id: '3',
+      name: item.name,
+      },
+    {
+      id: '4',
+      name: item.name,
+        }
+  )
+})
+
+console.log(items)
+console.log(results)
+console.log(newArray)
---
+++ callbacks.js
@@ -0,0 +1,49 @@
+/* A callback is a function that’s accepted as an argument and executed by another function (the higher-order function).
+
+There are 2 kinds of callback functions: synchronous and asynchronous.
+
+The synchronous callbacks are executed at the same time as the higher-order function that uses the callback. Synchronous callbacks are blocking. (eg. Array methods)
+
+Asynchronous callbacks are executed at a later time than the higher-order function. Asynchronous callbacks are non-blocking. */
+import axios from 'axios'
+
+const greet = name => `Hello, ${name}!` // This is our callback function
+
+console.log(greet('Thor'))
+
+const people = ['Thor', 'Hulk', 'Iron Man', 'Spider-Man']
+
+const greetings = people.map(greet)
+
+console.log(greetings)
+
+const fetchData = callback => {
+    setTimeout(() => callback('FetchData is Done'), 2000)
+}
+
+setTimeout(() => {
+    console.log(`Timer is done!`)
+    fetchData(text => {
+        console.log(text)
+    })
+}, 1500)
+
+console.log('Hello!')
+console.log('Hi!')
+
+const getPokemonData = () => { // TODO convert to async/await
+    return axios.get('https://pokeapi.co/api/v2/pokemon/snorlax').then(
+        ({ data }) => { // object destructuring
+            return data // becomes another Promise
+        }
+    ).catch( (error) => {
+        // handle the error
+        console.error(error)
+    })
+}
+
+getPokemonData().then(data => console.log(data))
+// console.log(getPokemonData())
+
+// TODO work with fs (file system module, writeFileSync and writeFile) :D
+// fs promises vs callback api
