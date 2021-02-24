de365f849106f4c6bdfa626d90f3ebf2808b212a
Thor Anderson <ta.anderson@gmail.com>
2021-02-24 15:14:29 -0700

    Got seed script working
    Co-authored-by: Torick Davis <torickjdavis@gmail.com>

--- seed/loadIMDB.js
+++ seed/loadIMDB.js
@@ -1,7 +1,16 @@
 import axios from "axios"
 import { Movie } from '../models/movie.js'
 import * as dotenv from 'dotenv'
+import mongoose from 'mongoose'
 dotenv.config()
+
+const seedMongo = async () => {
+  // Mongoose is not connected when the seed is run standalone, so start a new connection here
+  await mongoose.connect(`${process.env.DGM4790_CONNECTION_STRING}`,
+  {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+})
 
 const options = {
   method: 'GET',
@@ -11,22 +20,21 @@ const options = {
     'x-rapidapi-key': `${process.env.RAPIDAPI_KEY}`,
     'x-rapidapi-host': 'imdb8.p.rapidapi.com'
   }
-};
-
-axios.request(options).then(function (response) {
-    stuffMongo(response.data)
-    
-}).catch(function (error) {
-	console.error(error);
-})
-
-const stuffMongo = (movieList) => {
-    console.log(movieList.d[0])
-    addMovie(movieList.d[0])
+}
+  
+  try {
+    const response = await axios.request(options)
+    //console.log(response.data.d[0])
+    //await addMovie(response.data.d[0])
+    await addMovies(response.data.d)
+    await mongoose.connection.close() // close connection after all work is done
+  } catch (error) {
+    console.error(error)
+  }
+  
 }
 
-
-export const addMovie = ((oneMovie) => {
+const addMovie = async (oneMovie) => {
     const movie = new Movie({
         title: oneMovie.l,
         rank: oneMovie.rank,
@@ -34,7 +42,16 @@ export const addMovie = ((oneMovie) => {
         id: oneMovie.id,
         year: oneMovie.y
     })
-    console.log(movie)
-    movie.save() // save method is provided by Mongoose
-    //res.json(movie)
-})
+    //console.log(movie)
+    await movie.save() // save method is provided by Mongoose
+    console.log('Added successfuly')
+}
+
+const addMovies = async (movieList) => {
+  for (let movie of movieList) {
+    //console.log(movie)
+    await addMovie(movie)
+  }
+}
+
+seedMongo()
